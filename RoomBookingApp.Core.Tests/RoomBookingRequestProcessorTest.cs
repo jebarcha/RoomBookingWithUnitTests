c646a1875504e569a818ded2355f36788f1f3a2c
jebarcha <jebarcha@hotmail.com>
2024-08-08 17:03:31 -0600

    First Commit with unit tests of core

---
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -0,0 +1,38 @@
+﻿using RoomBookingApp.core.Models;
+using RoomBookingApp.core.Processors;
+using Shouldly;
+
+namespace RoomBookingApp.Core;
+
+public class RoomBookingRequestProcessorTest
+{
+    [Fact]
+    public void Should_Return_Room_Booking_Response_With_Request_Values()
+    {
+        // Arrange
+        var request = new RoomBookingRequest
+        {
+            FullName = "Test Name",
+            Email = "test@request.com",
+            Date = new DateTime(2024, 8, 8)
+        };
+
+        var processor = new RoomBookingRequestProcessor();
+
+        // Act
+        RoomBookingResult result = processor.BookRoom(request);
+
+        // Assert
+        //Assert.NotNull(result);
+        //result.ShouldNotBeNull();
+
+        //Assert.Equal(request.FullName, result.FullName);
+        //Assert.Equal(request.Email, result.Email);
+        //Assert.Equal(request.Date, result.Date);
+        //result.FullName.ShouldBe(result.FullName);
+        //result.Email.ShouldBe(result.Email);
+        //result.Date.ShouldBe(result.Date);
+
+        result.ShouldNotBeSameAs(request);
+    }
+}
---
+++ RoomBookingApp.Core/Models/RoomBookingResult.cs
@@ -0,0 +1,8 @@
+﻿namespace RoomBookingApp.core.Models;
+
+public class RoomBookingResult
+{
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public DateTime Date { get; set; }
+}
